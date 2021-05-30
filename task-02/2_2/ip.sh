30600aec6dc4c3334f03b9e7bf936664c35889f0
acidforest101 <51424491+acidforest101@users.noreply.github.com>
2021-05-30 16:55:57 +0300

    Delete task-02/2_2 directory

--- task-02/2_2/ip.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cont8080_ip=$(sudo lxc-info -i -n cont8080 | cut -d : -f 2)
-cont8081_ip=$(sudo lxc-info -i -n cont8081 | cut -d : -f 2)
-sudo iptables -F
-sudo iptables -t nat -I PREROUTING -i eth0 -p tcp --dport 8080 -j DNAT --to-destination ${cont8080_ip}:8080
-sudo iptables -t nat -I PREROUTING -i eth0 -p tcp --dport 8081 -j DNAT --to-destination ${cont8081_ip}:8081
